fd50f07d6350572ca4f4ba0444e4591594bed36a
karanmankodiya <karanmankodiya92@gmail.com>
2016-03-07 15:38:29 +0530

    Snipe IT Easy installation

---
+++ app/lang/nl/admin/accessories/message.php
@@ -0,0 +1,37 @@
+<?php
+
+return array(
+
+    'does_not_exist' => 'Accessoire bestaat niet.',
+    'assoc_users'	 => 'This accessory currently has :count items checked out to users. Please check in the accessories and and try again. ',
+
+    'create' => array(
+        'error'   => 'Accessory was not created, please try again.',
+        'success' => 'Accessoire succesvol aangemaakt.'
+    ),
+
+    'update' => array(
+        'error'   => 'Accessory was not updated, please try again',
+        'success' => 'Accessoire succesvol geupdate.'
+    ),
+
+    'delete' => array(
+        'confirm'   => 'Weet je zeker dat je deze categorie wilt verwijderen?',
+        'error'   => 'Er is een probleem opgetreden bij het verwijderen van deze categorie. Probeer opnieuw, aub.',
+        'success' => 'The category was deleted successfully.'
+    ),
+
+     'checkout' => array(
+        'error'   		=> 'Accessory was not checked out, please try again',
+        'success' 		=> 'Accessory checked out successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.'
+    ),
+
+    'checkin' => array(
+        'error'   		=> 'Accessory was not checked in, please try again',
+        'success' 		=> 'Accessory checked in successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.'
+    )
+
+
+);
---
+++ app/lang/ro/admin/accessories/message.php
@@ -0,0 +1,37 @@
+<?php
+
+return array(
+
+    'does_not_exist' => 'Accesoriul nu exista.',
+    'assoc_users'	 => 'This accessory currently has :count items checked out to users. Please check in the accessories and and try again. ',
+
+    'create' => array(
+        'error'   => 'Accesoriul nu a fost adaugat, va rugam incercati din nou.',
+        'success' => 'Accesoriu adaugat cu succes.'
+    ),
+
+    'update' => array(
+        'error'   => 'Accesoriul nu a fost actualizat, va rugam incercati din nou,',
+        'success' => 'Accesoriu actualizat cu succes.'
+    ),
+
+    'delete' => array(
+        'confirm'   => 'Sunteti sigur ca vreti sa stergeti aceasta categorie?',
+        'error'   => 'A aparut o problema la stergerea categoriei. Va rugam incercati din nou.',
+        'success' => 'Categoria a fost stearsa.'
+    ),
+
+     'checkout' => array(
+        'error'   		=> 'Accessory was not checked out, please try again',
+        'success' 		=> 'Accessory checked out successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.'
+    ),
+
+    'checkin' => array(
+        'error'   		=> 'Accessory was not checked in, please try again',
+        'success' 		=> 'Accessory checked in successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.'
+    )
+
+
+);
